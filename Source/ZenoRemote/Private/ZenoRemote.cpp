8e97a23c4cb9fcca335f778f4e8a901fca9c3a52
DarcJC <me@darc.pro>
2023-09-12 18:17:14 +0800

    Initial commit

---
+++ Source/ZenoRemote/Private/ZenoRemote.cpp
@@ -0,0 +1,15 @@
+﻿#include "ZenoRemote.h"
+
+#define LOCTEXT_NAMESPACE "FZenoRemoteModule"
+
+void FZenoRemoteModule::StartupModule()
+{
+}
+
+void FZenoRemoteModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FZenoRemoteModule, ZenoRemote)
